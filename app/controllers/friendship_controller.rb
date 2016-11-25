ef7b7ee75ade7e4a06a660ae9d43aabc217a0917
MAURICIO SUAZA <mauriciosuazauribe@gmail.com>
2016-11-25 03:28:29 -0500

    added methods for add decline  accept and block

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,26 +1,38 @@
 class FriendshipController < ApplicationController
 
   def add
-    
-    @user = User.find(params[:id])
 
+    @user = User.find(params[:id])
     current_user.request_friend(@user)
 
   end
 
   def accept
 
+    @user = User.find(params[:id])
+    #@user.request_friend(current_user)
+    current_user.accept_request(@user)
+
   end
 
   def decline
+
+    @user = User.find(params[:id])
+    current_user.decline_request(@user)
 
   end
 
   def remove
 
+    @user = User.find(params[:id])
+    current_user.remove_friend(@user)
+
   end
 
   def block
+
+    @user = User.find(params[:id])
+    current_user.block_friend(@user)
 
   end
 
