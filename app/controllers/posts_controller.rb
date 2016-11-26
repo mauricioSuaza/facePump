4b05b1f9e9e716ba4fac71256f88e038eb9305f4
MAURICIO SUAZA <mauriciosuazauribe@gmail.com>
2016-11-26 02:21:37 -0500

    edited posts likes behaviour

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -74,13 +74,13 @@ class PostsController < ApplicationController
   def upvote
     @post = Post.find(params[:id])
     @post.upvote_by current_user
-    redirect_to :back
+    redirect_to posts_path
   end
 
   def downvote
     @post = Post.find(params[:id])
     @post.downvote_by current_user
-    redirect_to :back
+    redirect_to posts_path
   end
 
 
